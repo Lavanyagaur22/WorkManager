fc227e468fe145fb80d3f3adaca1c0251ced84c3
Lavanya Gaur <lavanyagaur22@gmail.com>
2019-12-26 23:31:54 +0530

    code added

--- app/src/main/java/com/lavanya/workmanager/MainActivity.kt
+++ app/src/main/java/com/lavanya/workmanager/MainActivity.kt
@@ -1,12 +1,65 @@
 package com.lavanya.workmanager
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.work.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            nm.createNotificationChannel(
+                NotificationChannel(
+                    "first", "default",
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+            )
+        }
+
+        /*For scheduling a one time work request.
+         */
+        scheduleTask()
+
+        /*For scheduling a periodic time work request.
+        */
+//        scheduleRepeatingTasks()
+    }
+
+    private fun scheduleRepeatingTasks() {
+
+        /*Setting up different constraints on the work request.
+         */
+        val constraints = Constraints.Builder().apply {
+            setRequiredNetworkType(NetworkType.UNMETERED)
+            setRequiresCharging(true)
+            setRequiresDeviceIdle(false)
+            setRequiresStorageNotLow(true)
+        }.build()
+
+        val repeatingWork = PeriodicWorkRequestBuilder<NotificationRequestWorker>(
+            1,
+            TimeUnit.DAYS
+        ).setConstraints(constraints)
+            .build()
+        /*Enqueue the work request to an instance of Work Manager
+         */
+        WorkManager.getInstance(this).enqueue(repeatingWork)
+    }
+
+    private fun scheduleTask() {
+        val workerRequest = OneTimeWorkRequestBuilder<NotificationRequestWorker>()
+            .setInitialDelay(20, TimeUnit.SECONDS)
+            .build()
+
+        WorkManager.getInstance(this).enqueue(workerRequest)
     }
 }
---
+++ app/src/main/java/com/lavanya/workmanager/NotificationRequestWorker.kt
@@ -0,0 +1,24 @@
+package com.lavanya.workmanager
+
+import android.app.NotificationManager
+import android.content.Context
+import androidx.core.app.NotificationCompat
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class NotificationRequestWorker(val context: Context, workerParameters: WorkerParameters) :
+    Worker(context, workerParameters) {
+    override fun doWork(): Result {
+        val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val notification = NotificationCompat.Builder(context, "first").apply {
+            setContentTitle("Background task")
+            setContentText("Sample text")
+            setSmallIcon(R.drawable.ic_launcher_foreground)
+            priority = NotificationCompat.PRIORITY_DEFAULT
+        }.build()
+
+        nm.notify(System.currentTimeMillis().toInt(), notification)
+        return Result.success()
+    }
+}
